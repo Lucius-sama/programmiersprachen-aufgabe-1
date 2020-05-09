13fcdfb35ef02dbd969afce22385cee3c4a2d574
Lucius-sama <cornie_lol@yahoo.de>
2020-05-09 04:02:47 +0200

    kgV 1-20 Es ist 4:00...

--- source/Abgabe_1.cpp
+++ source/Abgabe_1.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 
+int kgv20()
+{
+    int kgv = -1;
+    int i = 20;
+    int tempSol = 0;
+    while (tempSol < 20){
+        i = i + 1;
+        for (int x = 1; x <= 20; x = x + 1) {
+            if (i % x == 0) {
+                tempSol = tempSol + 1;
+            }
+            else
+            {
+                tempSol = 0;
+                continue;
+            }
+        }
+
+
+    }
+     return i;
+ }
+
+
 int main()
 {
-  std::cout << "Hello, World!\n";
+   std::cout << kgv20();
+    
   return 0;
 }
